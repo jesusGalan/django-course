3d5ebf20d7562c1096f7f1b98468251b30e013af
jesus <jgc.developments@gmail.com>
2020-09-20 17:45:00 +0200

    Keep secrets under protection

--- biblioteca/biblioteca/settings.py
+++ biblioteca/biblioteca/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q-h3ebmww-_d&3hfvtwwt8t@n$o%+mkh#t&@9z((yf))iowcav'
+SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
